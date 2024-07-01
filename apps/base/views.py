95ebd1367bdbac2b83dea9a08ce1e7a33e03ad01
nur1slam09 <nur1234567891234567890@gmail.com>
2024-07-01 23:49:51 +0300

    First Commit

---
+++ apps/base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from apps.base.models import Base, Popular_category,Our_chef,News
+# Create your views here.
+
+def index(request):
+    base = Base.objects.latest('id')
+    category = Popular_category.objects.all()
+    chef = Our_chef.objects.all().order_by('?')[:3]
+    news = News.objects.all().order_by('?')[:2]
+    return render(request, 'index-dark.html', locals())
+
+
+
+
